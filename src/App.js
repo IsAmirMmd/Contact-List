2cb9f2ec38858d0f563be1e9cd691ae44a54b7a3
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-16 17:25:59 +0330

    save and load datas from Local Storage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
 import AddContact from "./Components/addContact/AddContact";
 import ContactList from "./Components/contactList/ContactList";
+import "./App.css";
 
 function App() {
   const [contacts, setContacts] = useState([]);
@@ -9,7 +9,7 @@ function App() {
   const addContactHandler = (contact) => {
     setContacts([
       ...contacts,
-      { id: Math.ceil(Math.random() * 100), ...contact },
+      { id: Math.ceil(Math.random() * 10000), ...contact },
     ]);
   };
 
@@ -17,6 +17,17 @@ function App() {
     const filteredConatcts = contacts.filter((contact) => contact.id !== id);
     setContacts([...filteredConatcts]);
   };
+
+  useEffect(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts) {
+      setContacts(savedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="App">
