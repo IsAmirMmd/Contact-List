d7b2d8a5f8e6da4d1ff5036f940499fae1a09644
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-07-16 14:57:28 +0330

    adding style to contatct lists

--- src/Components/contactList/ContactList.jsx
+++ src/Components/contactList/ContactList.jsx
@@ -1,12 +1,17 @@
+import "./ContactList.css";
+import userImage from "./../../assets/avatar.jpg";
+
 const ContactList = ({ contacts, onDelete }) => {
   return contacts.map((contact) => (
     <div key={contact.id} className="contact--child">
       <div className="contact--image">
-        <img src="" alt="profile" />
+        <img src={userImage} alt="profile" />
       </div>
       <div className="contact--user">
-        <p>{contact.name}</p>
-        <p>{contact.phone}</p>
+        <p>name : {contact.name}</p>
+        <a href={`tel:+98${contact.phone}`} className="contact--user__phone">
+          +98{contact.phone}
+        </a>
       </div>
       <button className="contact--btn" onClick={() => onDelete(contact.id)}>
         delete
